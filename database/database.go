597cbf1a31aec76fb7e93d19c8d42479d0abc777
Ignacio Moreno <ignacio@genuitec.com>
2022-06-11 13:21:03 -0500

    Fix mars options

--- database/database.go
+++ database/database.go
@@ -21,24 +21,24 @@ func newDatabaseBackupWorker() *DatabaseBackupWorker {
 
 func (worker *DatabaseBackupWorker) DoBackup() {
 	options := mars.NewOptions(
-		config.Conf.Get("app.hostname").(string),
-		config.Conf.Get("app.port").(string),
-		config.Conf.Get("app.username").(string),
-		config.Conf.Get("app.password").(string),
-		config.Conf.Get("app.database").(string),    // comma separated,
-		"",                                          // excluded databases
-		config.Conf.Get("app.dbthreshold").(int),    //dbthreshold
-		config.Conf.Get("app.tablethreshold").(int), // tablethreshold
-		config.Conf.Get("app.batchsize").(int),      // batchsize
-		false,                                       // forcesplit
-		"",                                          // additionals
-		1,                                           // verbosity
-		config.Conf.Get("app.mysqldumppath").(string),
-		config.Conf.Get("app.outdir").(string),
+		config.Conf.Get("database.hostname").(string),
+		config.Conf.Get("database.port").(string),
+		config.Conf.Get("database.username").(string),
+		config.Conf.Get("database.password").(string),
+		config.Conf.Get("database.database").(string), // comma separated,
+		"", // excluded databases
+		int(config.Conf.Get("database.dbthreshold").(int64)),    //dbthreshold
+		int(config.Conf.Get("database.tablethreshold").(int64)), // tablethreshold
+		int(config.Conf.Get("database.batchsize").(int64)),      // batchsize
+		false, // forcesplit
+		"",    // additionals
+		1,     // verbosity
+		config.Conf.Get("database.mysqldumppath").(string),
+		config.Conf.Get("database.outdir").(string),
 		true,
-		config.Conf.Get("app.dailyrotation").(int),
-		config.Conf.Get("app.weeklyrotation").(int),
-		config.Conf.Get("app.monthlyrotation").(int))
+		int(config.Conf.Get("database.dailyrotation").(int64)),
+		int(config.Conf.Get("database.weeklyrotation").(int64)),
+		int(config.Conf.Get("database.monthlyrotation").(int64)))
 
 	for _, db := range options.Databases {
 		mars.PrintMessage("Processing Database : "+db, options.Verbosity, mars.Info)
