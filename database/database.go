bef52315b8b2a77959d6671863d403bdc63ea959
Ignacio Moreno <ignacio@genuitec.com>
2022-07-04 10:05:05 -0500

    Remove code

--- database/database.go
+++ database/database.go
@@ -1,8 +1,19 @@
 package database
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"database/sql"
+	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"path"
+	"strconv"
+	"strings"
+	"time"
 
 	"playus/server-backup/config"
 	"playus/server-backup/directory"
@@ -12,9 +23,32 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/fatih/color"
 
-	mars "playus/server-backup/mars"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Options struct {
+	HostName                 string
+	Bind                     string
+	UserName                 string
+	Password                 string
+	Databases                []string
+	ExcludedDatabases        []string
+	DatabaseRowCountTreshold int
+	TableRowCountTreshold    int
+	BatchSize                int
+	ForceSplit               bool
+	AdditionalMySQLDumpArgs  string
+	Verbosity                int
+	MySQLDumpPath            string
+	OutputDirectory          string
+	DefaultsProvidedByUser   bool
+	ExecutionStartDate       time.Time
+	DailyRotation            int
+	WeeklyRotation           int
+	MonthlyRotation          int
+}
 
 type DatabaseBackupWorker struct {
 	S3Enabled bool
@@ -25,6 +59,17 @@ type DatabaseBackupWorker struct {
 	S3Key     string
 	Bucket    string
 }
+
+const (
+	// Info messages
+	Info = 1 << iota // a == 1 (iota has been reset)
+
+	// Warning Messages
+	Warning = 1 << iota // b == 2
+
+	// Error Messages
+	Error = 1 << iota // c == 4
+)
 
 var (
 	Worker = newDatabaseBackupWorker()
@@ -44,7 +89,7 @@ func newDatabaseBackupWorker() *DatabaseBackupWorker {
 }
 
 func (worker *DatabaseBackupWorker) DoBackup() {
-	options := mars.NewOptions(
+	options := NewOptions(
 		config.Conf.Get("database.hostname").(string),
 		config.Conf.Get("database.port").(string),
 		config.Conf.Get("database.username").(string),
@@ -65,22 +110,218 @@ func (worker *DatabaseBackupWorker) DoBackup() {
 		int(config.Conf.Get("database.monthlyrotation").(int64)))
 
 	for _, db := range options.Databases {
-		mars.PrintMessage("Processing Database : "+db, options.Verbosity, mars.Info)
+		PrintMessage("Processing Database : "+db, options.Verbosity, Info)
 
-		file, err := mars.GenerateSingleFileBackup(*options, db)
+		file, err := worker.GenerateSingleFileBackup(*options, db)
 		if file != nil && err == nil {
 			worker.upload(*file, *options)
 		}
 
-		mars.PrintMessage("Processing done for database : "+db, options.Verbosity, mars.Info)
+		PrintMessage("Processing done for database : "+db, options.Verbosity, Info)
 	}
-
-	// Backups retentions validation
-	mars.BackupRotation(*options)
 
 }
 
-func (worker *DatabaseBackupWorker) upload(file string, dbOptions mars.Options) {
+func (worker *DatabaseBackupWorker) GenerateSingleFileBackup(options Options, db string) (*string, error) {
+	PrintMessage("Generating single file backup : "+db, options.Verbosity, Info)
+
+	var args []string
+	args = append(args, fmt.Sprintf("-h%s", options.HostName))
+	args = append(args, fmt.Sprintf("-u%s", options.UserName))
+	args = append(args, fmt.Sprintf("-p%s", options.Password))
+
+	if options.AdditionalMySQLDumpArgs != "" {
+		args = append(args, strings.Split(options.AdditionalMySQLDumpArgs, " ")...)
+	}
+
+	timestamp := strings.Replace(strings.Replace(options.ExecutionStartDate.Format("2006-01-02"), "-", "", -1), ":", "", -1)
+	filename := path.Join(options.OutputDirectory, fmt.Sprintf("%s_%s.sql", db, timestamp))
+	os.MkdirAll(path.Dir(filename), os.ModePerm)
+
+	args = append(args, fmt.Sprintf("-r%s", filename))
+
+	args = append(args, db)
+	// args = append(args, "--column-statistics=0")
+
+	PrintMessage("mysqldump is being executed with parameters : "+strings.Join(args, " "), options.Verbosity, Info)
+
+	cmd := exec.Command(options.MySQLDumpPath, args...)
+	cmdOut, _ := cmd.StdoutPipe()
+	cmdErr, _ := cmd.StderrPipe()
+
+	cmd.Start()
+
+	output, _ := ioutil.ReadAll(cmdOut)
+	err, _ := ioutil.ReadAll(cmdErr)
+	cmd.Wait()
+
+	PrintMessage("mysqldump output is : "+string(output), options.Verbosity, Info)
+
+	if string(err) != "" && !strings.Contains(string(err), "Using a password on the command line interface can be insecure") {
+		PrintMessage("mysqldump error is: "+string(err), options.Verbosity, Error)
+		return nil, errors.New(string(err))
+	}
+
+	// Compressing
+	PrintMessage("Compressing table file : "+filename, options.Verbosity, Info)
+
+	// set up the output file
+	file, errcreate := os.Create(filename + ".tar.gz")
+
+	if errcreate != nil {
+		PrintMessage("error to create a compressed file: "+filename, options.Verbosity, Error)
+		return nil, errcreate
+	}
+
+	defer file.Close()
+	// set up the gzip writer
+	gw := gzip.NewWriter(file)
+	defer gw.Close()
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+
+	if errcompress := Compress(tw, filename); errcompress != nil {
+		PrintMessage("error to compress file: "+filename, options.Verbosity, Error)
+		return nil, errcompress
+	}
+
+	PrintMessage("Single file backup successfull : "+db, options.Verbosity, Info)
+	return &filename, nil
+}
+
+// Compress compresses files into tar.gz file
+func Compress(tw *tar.Writer, p string) error {
+	file, err := os.Open(p)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if stat, err := file.Stat(); err == nil {
+		// now lets create the header as needed for this file within the tarball
+		header := new(tar.Header)
+		header.Name = path.Base(p)
+		header.Size = stat.Size()
+		header.Mode = int64(stat.Mode())
+		header.ModTime = stat.ModTime()
+		// write the header to the tarball archive
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+		// copy the file data to the tarball
+		if _, err := io.Copy(tw, file); err != nil {
+			return err
+		}
+
+		// Removing the original file after zipping it
+		err = os.Remove(p)
+
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+	return nil
+}
+
+func PrintMessage(message string, verbosity int, messageType int) {
+	colors := map[int]color.Attribute{Info: color.FgGreen, Warning: color.FgHiYellow, Error: color.FgHiRed}
+
+	if verbosity == 2 {
+		color.Set(colors[messageType])
+		fmt.Println(message)
+		color.Unset()
+	} else if verbosity == 1 && messageType > 1 {
+		color.Set(colors[messageType])
+		fmt.Println(message)
+		color.Unset()
+	} else if verbosity == 0 && messageType > 2 {
+		color.Set(colors[messageType])
+		fmt.Println(message)
+		color.Unset()
+	}
+}
+
+func NewOptions(hostname string, bind string, username string, password string, databases string, excludeddatabases string, databasetreshold int, tablethreshold int, batchsize int, forcesplit bool, additionals string, verbosity int, mysqldumppath string, outputDirectory string, defaultsProvidedByUser bool, dailyrotation int, weeklyrotation int, monthlyrotation int) *Options {
+
+	databases = strings.Replace(databases, " ", "", -1)
+	databases = strings.Replace(databases, " , ", ",", -1)
+	databases = strings.Replace(databases, ", ", ",", -1)
+	databases = strings.Replace(databases, " ,", ",", -1)
+	dbs := strings.Split(databases, ",")
+	dbs = removeDuplicates(dbs)
+
+	excludeddbs := []string{}
+
+	if databases == "--all-databases" {
+
+		excludeddatabases = excludeddatabases + ",information_schema,performance_schema"
+
+		dbslist := GetDatabaseList(hostname, bind, username, password, verbosity)
+		databases = strings.Join(dbslist, ",")
+
+		excludeddatabases = strings.Replace(excludeddatabases, " ", "", -1)
+		excludeddatabases = strings.Replace(excludeddatabases, " , ", ",", -1)
+		excludeddatabases = strings.Replace(excludeddatabases, ", ", ",", -1)
+		excludeddatabases = strings.Replace(excludeddatabases, " ,", ",", -1)
+		excludeddbs := strings.Split(excludeddatabases, ",")
+		excludeddbs = removeDuplicates(excludeddbs)
+
+		// Databases to not be in the backup
+		dbs = difference(dbslist, excludeddbs)
+	}
+
+	return &Options{
+		HostName:                 hostname,
+		Bind:                     bind,
+		UserName:                 username,
+		Password:                 password,
+		Databases:                dbs,
+		ExcludedDatabases:        excludeddbs,
+		DatabaseRowCountTreshold: databasetreshold,
+		TableRowCountTreshold:    tablethreshold,
+		BatchSize:                batchsize,
+		ForceSplit:               forcesplit,
+		AdditionalMySQLDumpArgs:  additionals,
+		Verbosity:                verbosity,
+		MySQLDumpPath:            mysqldumppath,
+		OutputDirectory:          outputDirectory,
+		DefaultsProvidedByUser:   defaultsProvidedByUser,
+		ExecutionStartDate:       time.Now(),
+		DailyRotation:            dailyrotation,
+		WeeklyRotation:           weeklyrotation,
+		MonthlyRotation:          monthlyrotation,
+	}
+}
+
+// GetDatabaseList retrives list of databases on mysql
+func GetDatabaseList(hostname string, bind string, username string, password string, verbosity int) []string {
+	PrintMessage("Getting databases : "+hostname, verbosity, Info)
+
+	db, err := sql.Open("mysql", username+":"+password+"@tcp("+hostname+":"+bind+")/mysql")
+	checkErr(err)
+
+	defer db.Close()
+
+	rows, err := db.Query("SHOW DATABASES")
+	checkErr(err)
+
+	var result []string
+
+	for rows.Next() {
+		var databaseName string
+
+		err = rows.Scan(&databaseName)
+		checkErr(err)
+
+		result = append(result, databaseName)
+	}
+
+	PrintMessage(strconv.Itoa(len(result))+" databases retrived : "+hostname, verbosity, Info)
+
+	return result
+}
+
+func (worker *DatabaseBackupWorker) upload(file string, dbOptions Options) {
 	if !worker.S3Enabled {
 		return
 	}
@@ -121,4 +362,38 @@ func checkErr(err error) bool {
 		return true
 	}
 	return false
+}
+
+func removeDuplicates(elements []string) []string {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := []string{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v]] = true
+			// Append to result slice.
+			result = append(result, elements[v])
+		}
+	}
+	// Return the new slice.
+	return result
+}
+
+// difference returns the elements in a that aren't in b
+func difference(a, b []string) []string {
+	mb := map[string]bool{}
+	for _, x := range b {
+		mb[x] = true
+	}
+	ab := []string{}
+	for _, x := range a {
+		if _, ok := mb[x]; !ok {
+			ab = append(ab, x)
+		}
+	}
+	return ab
 }
